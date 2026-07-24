9c4d610bb0611b74ecdd6e2e3fe087ed3e1d1d87
eyakubsharif <eakubsharif8@gmail.com>
2026-07-24 00:12:51 -0700

    login form added

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,22 @@
 console.log('clicked index.js')
+
+const getValue =(id)=>{
+     const value = parseInt(document.getElementById(id).value)
+     return value
+}
+
+document.getElementById('loginBtn').addEventListener('click',function(e){
+    e.preventDefault()
+    const number =123
+    const pin = 111
+
+    const mobileNumber =getValue('number');
+    const pinNumber = getValue('pin')
+console.log(number,pin,mobileNumber)
+    if(mobileNumber == number && pinNumber == pin){
+        window.location.href ="home.html"
+    }else{
+      const errorMessage = document.getElementById('error');
+    errorMessage.innerText = 'Please give valid input';
+    }
+})
